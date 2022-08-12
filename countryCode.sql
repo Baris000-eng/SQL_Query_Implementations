dce84d2a3cf366ef5cd6f758e330be68ffd66723
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-12 15:36:29 +0300

    Update countryCode.sql

--- countryCode.sql
+++ countryCode.sql
@@ -3,5 +3,5 @@
     WHERE countrycode = 'USA' and population > 100000;
     
     
-//Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
-//The CITY table is described as follows:
+#Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+#The CITY table is described as follows:
