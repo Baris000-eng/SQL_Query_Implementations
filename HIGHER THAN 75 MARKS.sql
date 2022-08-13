68da0bf348c84cc9e4ec59a14bb276455eca75a0
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 18:25:56 +0300

    Update HIGHER THAN 75 MARKS.sql

--- HIGHER THAN 75 MARKS.sql
+++ HIGHER THAN 75 MARKS.sql
@@ -1,7 +1,6 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT st.Name
+FROM students st
+WHERE st.Marks > 75
+ORDER BY SUBSTR(st.Name, -3), st.id ASC;
 
-
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
