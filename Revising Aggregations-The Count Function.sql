924e9c94a769fb4179ff8341ad9baba17b615cb1
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 18:58:37 +0300

    Add files via upload

---
+++ Revising Aggregations-The Count Function.sql
@@ -0,0 +1,7 @@
+SELECT name
+FROM city
+WHERE countrycode = 'USA' and population > 120000;
+
+
+#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
+#The CITY table is described as follows: 
