7106a353abab7db5be93a51996e2e73c3776a1f1
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:56:39 +0300

    Update Weather Observations Station 18.sql

--- Weather Observations Station 18.sql
+++ Weather Observations Station 18.sql
@@ -1,7 +1,9 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT ROUND( (MAX(s.LAT_N)-MIN(s.LAT_N)+MAX(s.LONG_W)-MIN(s.LONG_W)) , 4 )
+FROM station s;
 
-
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Consider  P1(a,b) and  P2(c,d) to be two points on a 2D plane.
+ # a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ # b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ # c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ # d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+#Query the Manhattan Distance between points  (a,b) and  (c,d) and round it to a scale of  4 decimal places.
