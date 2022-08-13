451ea7316d747d5015fbdbd60cfeff95861a27a0
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:31:43 +0300

    Update TypeOfTriangle.sql

--- TypeOfTriangle.sql
+++ TypeOfTriangle.sql
@@ -1,7 +1,15 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT CASE WHEN 
+t.A > 0 AND t.B > 0 AND t.C > 0 AND t.A + t.B > t.C AND t.A + t.C > t.B AND 
+t.B + t.C > t.A THEN
+CASE 
+WHEN t.A = t.B AND t.B = t.C AND t.A = t.C THEN 'Equilateral'
+WHEN t.A = t.B OR t.A = t.C OR t.B = t.C THEN 'Isosceles'
+ELSE 'Scalene'
+END
+ELSE 'Not A Triangle'
+END FROM triangles t;
 
 
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Write a query identifying the type of each record 
+#in the TRIANGLES table using its three side lengths. 
+#Output one of the following statements for each record in the table:
