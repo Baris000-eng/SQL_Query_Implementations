7d73ecd2b673fbd8667345ce2797d753eaadab94
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:44:33 +0300

    Update Average Population.sql

--- Average Population.sql
+++ Average Population.sql
@@ -1,7 +1,5 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT FLOOR(AVG(ct.population))
+FROM city ct;
 
 
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query the average population for all cities in CITY, rounded down to the nearest integer.
