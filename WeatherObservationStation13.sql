7b5d638996fccb1feea2db0be7f7712889c1bbff
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:21:47 +0300

    Update WeatherObservationStation13.sql

--- WeatherObservationStation13.sql
+++ WeatherObservationStation13.sql
@@ -1,7 +1,6 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT TRUNCATE(SUM(st.LAT_N),4)
+FROM station st
+WHERE st.LAT_N > 38.7880 AND st.LAT_N < 137.2345;
 
 
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  38.7880 and less than 137.2345. Truncate your answer to  4 decimal places.
