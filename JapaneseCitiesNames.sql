aa6c05f26419de1ebfe0fe88b4ea8d3e684c4a10
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 17:24:35 +0300

    Update JapaneseCitiesNames.sql

--- JapaneseCitiesNames.sql
+++ JapaneseCitiesNames.sql
@@ -1,7 +1,7 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT ct.name
+FROM city ct
+WHERE ct.countrycode = 'JPN';
 
 
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
+#Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN. 
 #The CITY table is described as follows: 
