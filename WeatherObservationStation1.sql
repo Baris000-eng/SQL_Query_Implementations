67742a1e9b9727424b41e7114f933c54b7bb4ff8
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 17:29:52 +0300

    Update WeatherObservationStation1.sql

--- WeatherObservationStation1.sql
+++ WeatherObservationStation1.sql
@@ -1,7 +1,5 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT st.city,st.state
+FROM station st;
 
 
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query a list of CITY and STATE from the STATION table. 
