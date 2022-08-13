075647d2c6473efac4fff88e03c59c8aea05c891
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:39:36 +0300

    Update Revising Aggregations-The Count Function.sql

--- Revising Aggregations-The Count Function.sql
+++ Revising Aggregations-The Count Function.sql
@@ -1,7 +1,5 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT COUNT(ct.Name)
+FROM city ct
+WHERE ct.population > 100000;
 
-
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query a count of the number of cities in CITY having a Population larger than 100000
