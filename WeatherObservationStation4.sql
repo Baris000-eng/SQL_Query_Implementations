391da31ab2622d2376763f3d3190ea761e95abac
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 17:32:52 +0300

    Update WeatherObservationStation4.sql

--- WeatherObservationStation4.sql
+++ WeatherObservationStation4.sql
@@ -1,7 +1,4 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT COUNT(st.city) - COUNT(DISTINCT(st.city))
+FROM station st;
 
-
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. 
