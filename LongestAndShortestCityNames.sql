705b770c3f97bb5a9ee4a74853b89ea62bc1be7b
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 20:31:30 +0300

    Update LongestAndShortestCityNames.sql

--- LongestAndShortestCityNames.sql
+++ LongestAndShortestCityNames.sql
@@ -1,7 +1,17 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT st.city,CHAR_LENGTH(st.city)
+FROM station st
+WHERE LENGTH(st.city) = (SELECT MAX(CHAR_LENGTH(s1.city)) FROM station s1)
+ORDER BY st.city,CHAR_LENGTH(st.city) DESC
+LIMIT 1;
+
+SELECT st.city,CHAR_LENGTH(st.city)
+FROM station st
+WHERE LENGTH(st.city) = (SELECT MIN(CHAR_LENGTH(s1.city)) FROM station s1)
+ORDER BY st.city,CHAR_LENGTH(st.city) ASC
+LIMIT 1;
 
 
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query the two cities in STATION with the shortest and longest CITY names, 
+#as well as their respective lengths (i.e.: number of characters in the name). 
+#If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically. 
+#The STATION table is described as follows:
