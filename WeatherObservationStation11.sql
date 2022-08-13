8882f3a9099c69b17b5e11eaa276c9a8fcc9dfeb
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 18:12:11 +0300

    Update WeatherObservationStation11.sql

--- WeatherObservationStation11.sql
+++ WeatherObservationStation11.sql
@@ -1,3 +1,3 @@
-SELECT DISTINCT st.city FROM STATION st WHERE RIGHT(st.city, 1) NOT IN ('A','E','I','O','U') OR LEFT(st.city,1) NOT IN ('A','E','I','O','U');
+SELECT DISTINCT st.city FROM STATION st WHERE RIGHT(st.city, 1) NOT IN ('a','e','i','o','u') OR LEFT(st.city,1) NOT IN ('A','E','I','O','U');
 
 #Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
