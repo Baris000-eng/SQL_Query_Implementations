5a2dd19ba85aa0e41c5f0dc5496c621a57e2d433
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:18:22 +0300

    Update WeatherObservationStation14.sql

--- WeatherObservationStation14.sql
+++ WeatherObservationStation14.sql
@@ -2,4 +2,4 @@ SELECT TRUNCATE(MAX(st.LAT_N),4)
 FROM station st
 WHERE st.LAT_N < 137.2345;
 
-Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to  decimal places.
+# Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to  decimal places.
