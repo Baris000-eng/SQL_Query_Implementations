d26ba225e80f5639d6857d3ad808f0af23250a95
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:21:57 +0300

    Update WeatherObservationStation13.sql

--- WeatherObservationStation13.sql
+++ WeatherObservationStation13.sql
@@ -3,4 +3,5 @@ FROM station st
 WHERE st.LAT_N > 38.7880 AND st.LAT_N < 137.2345;
 
 
-#Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  38.7880 and less than 137.2345. Truncate your answer to  4 decimal places.
+#Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  
+#38.7880 and less than 137.2345. Truncate your answer to  4 decimal places.
