cf4fe89a09dea8f0711bd5ff352d8624be344255
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:41:18 +0300

    Update Population Density Difference.sql

--- Population Density Difference.sql
+++ Population Density Difference.sql
@@ -1,7 +1,4 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+select MAX(ct.population) - MIN(ct.population)
+FROM city ct;
 
-
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query the difference between the maximum and minimum populations in CITY.
