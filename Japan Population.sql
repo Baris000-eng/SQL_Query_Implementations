eea4450ad0513aba337eb8ee044acdceee284ee3
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 19:42:45 +0300

    Update Japan Population.sql

--- Japan Population.sql
+++ Japan Population.sql
@@ -1,7 +1,5 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT SUM(ct.population)
+FROM city ct
+WHERE ct.countrycode = 'JPN';
 
-
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
