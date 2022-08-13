58698f307a3a85a2046dce4d7cf1b48c82765099
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2022-08-13 17:35:47 +0300

    Update WeatherObservationStation6.sql

--- WeatherObservationStation6.sql
+++ WeatherObservationStation6.sql
@@ -1,7 +1,7 @@
-SELECT name
-FROM city
-WHERE countrycode = 'USA' and population > 120000;
+SELECT DISTINCT st.city
+FROM station st
+WHERE st.city LIKE 'A%' OR st.city LIKE 'E%' OR st.city LIKE 'I%' OR 
+st.city LIKE 'O%' OR st.city LIKE 'U%';
 
 
-#Query the NAME field for all American cities in the CITY table #with populations larger than 120000. The CountryCode for #America is USA.
-#The CITY table is described as follows: 
+#Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
